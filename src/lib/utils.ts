06bb637e352783a5d79f8f4c25bc7b86761733f4
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-08-09 17:22:29 +0700

    chore!: remove old local storage based reducers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,15 +6,6 @@ export function cn(...inputs: ClassArray): string {
 	return twMerge(clsx(inputs));
 }
 
-export function slugify(text: string): string {
-	return text
-		.toLowerCase()
-		.normalize("NFKD")
-		.replaceAll(/[̀-ͯ]/g, "")
-		.replaceAll(/[^a-z0-9]+/g, "-")
-		.replaceAll(/(^-|-$)/g, "");
-}
-
 export function rupiah(n: number): string {
 	return `Rp ${n.toLocaleString("id-ID")}`;
 }
--- src/store/reducers/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-
-import auth from "./auth";
-import orders from "./orders";
-
-export const rootReducer = combineReducers({ auth, orders });
